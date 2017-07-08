697bd4f3ace7e3c9c2c486434bd2eb5de6435472
Alessandro <alessandro.budroni@miracl.com>
2017-07-08 19:59:20 +0100

    Add Licence.

--- remove_allignments_bits.m
+++ remove_allignments_bits.m
@@ -1,4 +1,20 @@
-%%Remove the various finder puttin the null value
+% Copyright 2017
+% 
+% Authors: Alessandro Budroni, Ermes Franch, Giuseppe Giffone
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+% 
+%     http://www.apache.org/licenses/LICENSE-2.0
+% 
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
+% Remove the various finder puttin the null value
 function [ qr_matrix ] = remove_allignments_bits( qr_matrix, version)
 %value for pointless bit
 global null;
